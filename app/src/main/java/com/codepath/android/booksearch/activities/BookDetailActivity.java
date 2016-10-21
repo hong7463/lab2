2450b2ed05e5743e74149aa3fc3af8a75317fc86
hh1141 <hh1141@nyu.edu>
2016-10-20 20:40:35 -0700

    first commit

--- app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
@@ -1,18 +1,39 @@
 package com.codepath.android.booksearch.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.codepath.android.booksearch.R;
+import com.codepath.android.booksearch.models.Book;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class BookDetailActivity extends AppCompatActivity {
     private ImageView ivBookCover;
     private TextView tvTitle;
     private TextView tvAuthor;
+    private Toolbar toolbar;
+    private ShareActionProvider shareActionProvider;
+    private Book book;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +43,67 @@ public class BookDetailActivity extends AppCompatActivity {
         ivBookCover = (ImageView) findViewById(R.id.ivBookCover);
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvAuthor = (TextView) findViewById(R.id.tvAuthor);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        book = getIntent().getParcelableExtra("book");
 
         // Extract book object from intent extras
+        Picasso.with(this).load(book.getCoverUrl()).into(ivBookCover, new Callback() {
+            @Override
+            public void onSuccess() {
+                prepareShareInten();
+                attachShareIntentAction();
+            }
+
+            @Override
+            public void onError() {
+
+            }
+        });
+        tvTitle.setText(book.getTitle());
+        tvAuthor.setText(book.getAuthor());
+        getSupportActionBar().setTitle(book.getTitle());
 
         // Use book object to populate data into views
+
+    }
+
+    private void prepareShareInten() {
+        Uri bmpUri = getLocalBitMapUri(ivBookCover);
+        intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        intent.setType("image/*");
+    }
+
+    private void attachShareIntentAction() {
+        if(shareActionProvider != null && intent != null) {
+            shareActionProvider.setShareIntent(intent);
+        }
+    }
+
+    private Uri getLocalBitMapUri(ImageView imageView) {
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if(drawable instanceof BitmapDrawable) {
+            bmp = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+        }
+        else {
+            return null;
+        }
+
+        Uri bmpUri = null;
+        try {
+            File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, fileOutputStream);
+            fileOutputStream.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
     }
 
 
@@ -33,6 +111,13 @@ public class BookDetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_book_detail, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+//        intent = new Intent(Intent.ACTION_SEND);
+//        intent.setType("text/plain");
+//        intent.putExtra(Intent.EXTRA_TEXT, book.getCoverUrl());
+//        shareActionProvider.setShareIntent(intent);
+        attachShareIntentAction();
         return true;
     }
 
@@ -44,10 +129,10 @@ public class BookDetailActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
